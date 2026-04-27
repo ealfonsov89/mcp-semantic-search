15a53ce286ebd6d70832e36598cc1424b11c9b14
Erneto Alfonso <ealfonsov89@users.noreply.github.com>
2026-04-27 17:47:26 +0000

    fix: update indexing path to absolute for consistency

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ use log::info;
 mod full_indexing;
 
 fn main() {
-    full_indexing::start_indexing("./src").unwrap();
+    full_indexing::start_indexing("/workspaces/mcp-semantic-search/src").unwrap();
 }
